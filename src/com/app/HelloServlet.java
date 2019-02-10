f1dd567e805d167514078053085c9bb1ea8fe405
TEJ DEEP <TEJ DEEP@192.168.0.20>
2019-02-10 13:23:21 +0530

    commited

---
+++ src/com/app/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloServlet  extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) 
+			throws ServletException, IOException {
+		resp.setContentType("text/html");
+	PrintWriter out=resp.getWriter();
+	out.println("<marquee><h2>gggggg<h2><marquee>");
+	}
+
+}
